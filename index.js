0f2ff2f2d32649970f1eba12bb729d98a59a6095
Camilo Pinto <camilo.pinto1@mail.udp.cl>
2023-06-24 18:50:41 -0400

    mongodb client partial implementation

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 
-mongoose.connect('mongodb://127.0.0.1:27017/mongo');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://publicGithubAuth:3HQaWMwhAu7MVi4n@devweb.or5phdi.mongodb.net/?retryWrites=true&w=majority";
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("apibank").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 const app = express();
 app.use(express.json());
-
-var db = [];
 
 app.get('/', (req, res) => {
   res.json(db);
